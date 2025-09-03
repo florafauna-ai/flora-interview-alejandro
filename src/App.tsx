7af5902faf0eee4892bf306f0385a4c1c20224a8
Alex Carrizosa <alex.carrizosa@outlook.com>
2025-09-03 15:23:54 -0600

    Added aspect ratio change and dimension select

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,34 @@ const aspectRatios: AspectRatio[] = [
   { label: "21:9 (Ultrawide)", value: 2.33 },
 ];
 
+const maintainDimensions: string[] = [
+  "width",
+  "height"
+]
+
+const getSanitizedAspectRatio = (aspectRatio: string | number) => {
+  if (typeof aspectRatio === "number")
+    return aspectRatio
+  if (typeof aspectRatio === "string") {
+    if (aspectRatio.includes(":")) {
+      const [width, height] = aspectRatio.split(":").map(Number)
+      return width / height
+    }
+    else if (aspectRatio.includes("/")) {
+      const [width, height] = aspectRatio.split("/").map(Number)
+      return width / height
+    }
+    else if(aspectRatio == "1")
+      return 1
+  }
+  throw new Error("Value not string or number")
+}
+
+const getAspectRatioCoefficient = (original: AspectRatio, newAspectRatio: AspectRatio) => {
+  const sanitizedOriginalRatio = getSanitizedAspectRatio(original.value)
+  const sanitizedNewAspectRatio = getSanitizedAspectRatio(newAspectRatio.value)
+  return sanitizedOriginalRatio / sanitizedNewAspectRatio
+}
 
 // Component that will be rendered inside the 3D scene
 const ResizableDiv = ({
@@ -56,19 +84,42 @@ function App() {
     aspectRatios[0]
   );
   const [dimensions, setDimensions] = useState({ width: 300, height: 300 });
+  const [originalDimensions, setOriginalDimensions] = useState({ width: 300, height: 300})
+
+  const [selectedDimension, setSelectedDimension] = useState<string>("width")
+
+  const handleDimensionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(`selected dimension: ${event.currentTarget.value}`)
+    setSelectedDimension(event.currentTarget.value)
+  }
 
   const handleRatioChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     // handle drag - no override with shift
     // get value and search on the aspecratios enum
-    const selectedAspectRatio = event.currentTarget.value;
+    const selectedAspectRatio = event.currentTarget.value
+    console.log(`Selected aspect ratio ${selectedAspectRatio}`)
     aspectRatios.forEach((aspectRatio) => {
-      if (aspectRatio.value == selectedAspectRatio)
+      if (aspectRatio.value == selectedAspectRatio) {
         setSelectedRatio(aspectRatio)
+        const coefficient = getAspectRatioCoefficient(aspectRatios[0], aspectRatio)
+        switch(selectedDimension) {
+          case "width":
+            setDimensions({ width: originalDimensions.width, height: originalDimensions.height * coefficient})
+            break
+          
+          case "height":
+            setDimensions({ width: originalDimensions.width * coefficient, height: originalDimensions.height})
+            break
+
+          default:
+            throw new Error("No valid dimension selected")
+        }
+      }
     })
   };
 
   const handleDrag = useCallback(
-    (e: React.MouseEvent) => {},
+    (e: React.MouseEvent) => { },
     [dimensions, selectedRatio]
   );
 
@@ -84,6 +135,17 @@ function App() {
           {aspectRatios.map((ratio) => (
             <option key={ratio.value} value={ratio.value}>
               {ratio.label}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="maintain"> Maintain dimension</label>
+        <select 
+          id="maintain"
+          value={selectedDimension}
+          onChange={handleDimensionChange}>
+          {maintainDimensions.map((dimensions) => (
+            <option key={dimensions} value={dimensions}>
+              {dimensions}
             </option>
           ))}
         </select>
