45973ed64f94f8dedce1e2c321416a048e8ab6c0
Alex Carrizosa <alex.carrizosa@outlook.com>
2025-09-03 16:37:57 -0600

    Added drag functionality, added aspect ratio resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Html } from "@react-three/drei";
 import "./App.css";
@@ -16,10 +16,14 @@ const aspectRatios: AspectRatio[] = [
   { label: "21:9 (Ultrawide)", value: 2.33 },
 ];
 
+// I added this small functionality to be able to select which of the dimensions to keep on the coefficient
 const maintainDimensions: string[] = [
   "width",
   "height"
 ]
+
+// This is why I wanted to get ahold of the type, my reasoning was to discriminate based on string vs number and just do calculations if string had to be parsed
+// Also coefficient is 'standardized' so it could also maintain better proportions on other more complex aspext ratios
 
 const getSanitizedAspectRatio = (aspectRatio: string | number) => {
   if (typeof aspectRatio === "number")
@@ -33,12 +37,13 @@ const getSanitizedAspectRatio = (aspectRatio: string | number) => {
       const [width, height] = aspectRatio.split("/").map(Number)
       return width / height
     }
-    else if(aspectRatio == "1")
+    else if (aspectRatio == "1")
       return 1
   }
   throw new Error("Value not string or number")
 }
 
+// Helper function to modify old vs new aspect ratios i.e. 1:1 -> 4:3
 const getAspectRatioCoefficient = (original: AspectRatio, newAspectRatio: AspectRatio) => {
   const sanitizedOriginalRatio = getSanitizedAspectRatio(original.value)
   const sanitizedNewAspectRatio = getSanitizedAspectRatio(newAspectRatio.value)
@@ -84,31 +89,33 @@ function App() {
     aspectRatios[0]
   );
   const [dimensions, setDimensions] = useState({ width: 300, height: 300 });
-  const [originalDimensions, setOriginalDimensions] = useState({ width: 300, height: 300})
+  const [originalDimensions, setOriginalDimensions] = useState({ width: 300, height: 300 })
+  const [selectedFixedDimension, setSelectedFixedDimension] = useState<string>("width")
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+  const [startPosX, setStartPosX] = useState<number>(0)
+  const [startPosY, setStartPosY] = useState<number>(0)
 
-  const [selectedDimension, setSelectedDimension] = useState<string>("width")
-
+  // Adhering to the handleRatioChange function I also created handleDimensionChange to switch values based on the dropdown I added
   const handleDimensionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     console.log(`selected dimension: ${event.currentTarget.value}`)
-    setSelectedDimension(event.currentTarget.value)
+    setSelectedFixedDimension(event.currentTarget.value)
   }
 
   const handleRatioChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    // handle drag - no override with shift
-    // get value and search on the aspecratios enum
     const selectedAspectRatio = event.currentTarget.value
     console.log(`Selected aspect ratio ${selectedAspectRatio}`)
     aspectRatios.forEach((aspectRatio) => {
       if (aspectRatio.value == selectedAspectRatio) {
         setSelectedRatio(aspectRatio)
-        const coefficient = getAspectRatioCoefficient(aspectRatios[0], aspectRatio)
-        switch(selectedDimension) {
+        const coefficient = getAspectRatioCoefficient(aspectRatios[0], selectedRatio)
+        //There is probably a better way using useReducer to avoid the switch 
+        switch (selectedFixedDimension) {
           case "width":
-            setDimensions({ width: originalDimensions.width, height: originalDimensions.height * coefficient})
+            setDimensions({ width: originalDimensions.width, height: originalDimensions.height * coefficient })
             break
-          
+
           case "height":
-            setDimensions({ width: originalDimensions.width * coefficient, height: originalDimensions.height})
+            setDimensions({ width: originalDimensions.width * coefficient, height: originalDimensions.height })
             break
 
           default:
@@ -118,8 +125,67 @@ function App() {
     })
   };
 
+  //I tried using only onDrag but I was not able to extract start and end positions reliably so I defaulted to normal mouse events vs reacts
+  const handleMouseDown = useCallback((e: React.MouseEvent) => {
+    e.preventDefault()
+    setIsDragging(true)
+    setStartPosX(e.clientX)
+    setStartPosY(e.clientY)
+  }, [dimensions])
+
+  const handleMouseUp = useCallback(() => { setIsDragging(false) }, [dimensions])
+
+  useEffect(() => {
+    if (isDragging) {
+      window.addEventListener("mousemove", handleDrag)
+      window.addEventListener("mouseup", handleMouseUp)
+    }
+    return () => {
+      window.removeEventListener("mousemove", handleDrag)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [isDragging, handleMouseDown, setDimensions])
+
   const handleDrag = useCallback(
-    (e: React.MouseEvent) => { },
+    (e: MouseEvent) => {
+      setOriginalDimensions({ width: dimensions.width, height: dimensions.height })
+      const deltaX = e.clientX - startPosX
+      const deltaY = e.clientY - startPosY
+      console.log(`Dragging x:${e.clientX}, y: ${e.clientY}`)
+
+      const minimumXValue = 30
+      const minimumYValue = 30
+
+      //I was going to go with the normal key events but mouse event has a modifier listener I could use
+      if (e.shiftKey) {
+        console.log("Shift key pressed!")
+        const coefficient = getAspectRatioCoefficient(aspectRatios[0], selectedRatio)
+        let newWidth = 0
+        let newHeight = 0
+        // I know probably a switch statement for 2 cases is overkill, but I wanted to add the posibility later to also account for keeping both dimensions constrained, or none
+        switch (selectedFixedDimension) {
+          case "width":
+            newWidth = Math.max(minimumXValue, dimensions.width + deltaX)
+            setDimensions({
+              width: newWidth,
+              height: newWidth / coefficient
+            })
+            break
+
+          case "height":
+            newHeight = Math.max(minimumYValue, dimensions.height + deltaY)
+            setDimensions({
+              width: newHeight / coefficient,
+              height: newHeight
+            })
+            break
+        }
+      } else {
+        const maxWidth = Math.max(minimumXValue, dimensions.width + deltaX)
+        const maxHeight = Math.max(minimumYValue, dimensions.height + deltaY)
+        setDimensions({ width: maxWidth, height: maxHeight })
+      }
+    },
     [dimensions, selectedRatio]
   );
 
@@ -139,9 +205,9 @@ function App() {
           ))}
         </select>
         <label htmlFor="maintain"> Maintain dimension</label>
-        <select 
+        <select
           id="maintain"
-          value={selectedDimension}
+          value={selectedFixedDimension}
           onChange={handleDimensionChange}>
           {maintainDimensions.map((dimensions) => (
             <option key={dimensions} value={dimensions}>
@@ -168,7 +234,7 @@ function App() {
             left: 0,
           }}
         >
-          <ResizableDiv dimensions={dimensions} onDrag={handleDrag} />
+          <ResizableDiv dimensions={dimensions} onDrag={handleMouseDown} />
         </Canvas>
       </div>
     </div>
